56d54b34ed4a45f6a2323903c783ee4c83697e29
“Rodrigo <rodrigodias1805@gmail.com>
2024-02-11 21:49:47 +0000

    git

---
+++ app.js
@@ -0,0 +1,38 @@
+alert('Boas vindas ao jogo do numero secreto');
+let numeromaximo = 100;
+let numeroSecreto = parseInt(Math.random() *10 + 1);
+
+// console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// Enquanto não acerta
+while (chute!= numeroSecreto) {
+    chute = prompt(`escolha um numero de 1 a ${numeromaximo}`);   
+    if (chute == numeroSecreto ) {
+        break;
+    }else{ 
+        if (chute>numeroSecreto){
+            alert (`seu numero é maior que o ${numeroSecreto}`);
+        } else{
+            alert (`seu numero é menor que o ${numeroSecreto}`);
+    
+        }
+        tentativas ++;
+    }
+
+}
+
+let palavraTentativa  = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert (`Você descobriu o nume secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+
+// if ( tentativas > 1){
+//     alert (`Você descobriu o nume secreto ${numeroSecreto} com ${tentativas}tentativas`);
+
+
+// } else{
+//     alert (`Você descobriu o nume secreto ${numeroSecreto} com ${tentativas}tentativa`);
+
+
+// }
